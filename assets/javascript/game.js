d5338f5b3ee1aa2e0ca0a8ae78a82aae0c6f5b16
ScrantonPete <scrantonpete@yahoo.com>
2019-05-10 23:15:34 -0400

    random number working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,38 @@
 window.onload = function () {
     beginGame();
 }
+// Global Variables
+// ==============================================================================
 
 var movies = ["Godfather", "Step Brothers", "Scarface", "Indiana Jones and the Temple of Doom", "Ghostbusters", "Caddyshack", "What About Bob?"]
 var letters = "abcdefghijklmnopqrstuvwxyz?".split("");
 
-var currentMovie;
+var currentMovie = "";
 var matchingLetter = [];
 var wrongLetter = [];
+var totalguesses = 9;
+var guessLeft = 0;
+var gameOver = false;
 var wrongChoice;
 var wins = 0;
 var losses = 0;
+// Functions
+// ==============================================================================
+
+// Blank letters
+// function prepareBlanks() {
+//     spaces =
+// }
+
 
 function beginGame() {
-    var movieChoice = document.getElementById("movie");
-    movieChoice.innerHTML = "";
-    document.getElementById("")
+    guessLeft = totalguesses;
+
+    // random movie selector
+    currentMovie = Math.floor(Math.random() * (movies.length));
+    console.log(currentMovie);
 }
+
+// Main
+// ==============================================================================
+beginGame;
