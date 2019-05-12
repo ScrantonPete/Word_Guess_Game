19eb97321e9cf3b3dfdaf00a5897cfbb3fc6a562
ScrantonPete <scrantonpete@yahoo.com>
2019-05-11 22:17:27 -0400

    uploaded basic shell of Movie Hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,102 @@
 // Global Variables
 // ==============================================================================
 
-var movieList = ["godfather", "step brothers", "scarface", "indiana jones and the temple of doom", "ghostbusters", "caddyshack", "what about bob"]
+// Arrays and Variables
+var movieList = ["inception", "goodfellas", "scarface", "", "ghostbusters", "caddyshack", "jumanji", "airplane"]
 var selectedMovie = "";
 var lettersInMovie = [];
 var numBlanks = 0;
 var blanksAndSuccesses = [];
-var wrongLetter = [];
+var wrongLetters = [];
 
+// Game Counters
 var guessesLeft = 9;
 var winCount = 0;
-var losseCount = 0;
+var lossCount = 0;
 
 // Functions
 // ==============================================================================
-// user begins game by pressing anykey
-// computer displays underscores in place of letters of selected movie
-// user presses any letter key and computer reveals correct guesses or incorrect guesses are placed in a row of incorrect guesses and guesses remaining reduces to 1
-// if guesses remaining reaches 0, then whomp whomp movie plays and loses goes up by 1
-// if user guesses all correct letters then winning movie plays and wins goes up by 1
+
 function startGame() {
     selectedMovie = movieList[Math.floor(Math.random() * movieList.length)];
     lettersInMovie = selectedMovie.split("");
+    numBlanks = lettersInMovie.length;
+
+    // Reset counters
+    guessesleft = 9;
+    wrongletters = [];
+    blanksAndSuccesses = [];
+
+    // Populate blanks and successes with right number of blanks
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
+    }
+
+    // HTML updates are made 
+    document.getElementById("movieToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+
+
+    // Testing
 
     console.log(selectedMovie);
     console.log(lettersInMovie);
+    console.log(numBlanks);
+    console.log(blanksAndSuccesses);
+}
+// Check if letters are in Movie name
+function checkLetters(letter) {
+    var isLetterInMovie = false;
+
+    for (var i = 0; i < numBlanks; i++) {
+        if (selectedMovie[i] == letter) {
+            isLetterInMovie = true;
+        }
+    }
+    // check where in the movie that letter exists and then poplate blanksAndSuccesses array
+    if (isLetterInMovie) {
+        // populate blank with letter
+        for (var i = 0; i < numBlanks; i++) {
+            if (selectedMovie[i] == letter) {
+                blanksAndSuccesses[i] = letter;
+            }
+        }
+    }
+
+    // place wrong guess in wrongLetters array
+    else {
+        wrongLetters.push(letter);
+        guessesLeft--
+    }
+    // testing
+    console.log(blanksAndSuccesses);
+}
+function roundComplete() {
+    console.log("Win Count " + winCount + " | Loss Count " + lossCount + " | Guesses Left " + guessesLeft);
+
+
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("movieToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
+
+    if (lettersInMovie.toString() == blanksAndSuccesses.toString()) {
+        winCount++;
+        alert("You won");
+        document.getElementById("winCounter").innerHTML = winCount;
+        startGame();
+    }
+
+
+    else if (guessesLeft == 0) {
+        lossCount++;
+        alert("you Lost!");
+
+        document.getElementById("lossCounter").innerHTML = lossCount;
+        startGame();
+    }
+
 }
 
 
@@ -32,6 +104,15 @@ function startGame() {
 // Main Process
 // ==============================================================================
 
-
-
+// Game begins
 startGame();
+// Register key clicks
+
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+
+    // Testing
+    console.log(letterGuessed);
+}
---
+++ assets/readme.md
@@ -0,0 +1,22 @@
+Psuedo Code for Word Guess Game
+
+user begins game by pressing anykey
+computer selects a random movie from a predesigned array that has movies with number of guesses reamaining
+computer displays underscores in place of letters of selected movie
+user presses any letter key and computer reveals correct guesses or incorrect guesses are placed in a row of incorrect guesses and guesses remaining reduces to 1
+if guesses remaining reaches 0, then  loses goes up by 1
+if user guesses all correct letters wins goes up by 1
+
+
+This is a basic shell for a Movie Hangman game.
+
+It lets you guess the movie titles of some great movie titles.
+
+The project is useful to demonstrate some basid javascript functionality.
+
+You Can go to this GitHub page to play:
+
+
+You probably can't get help from me, lol!
+
+Maintained lossely by Pete Rezanka who is still new to coding
